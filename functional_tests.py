a71cffaa874f80e6cab98f1143964de59348fff5
K.Bakalarski <devops5000@gmail.com>
2019-05-18 23:17:02 +0200

    W komentarzach podano pierwsza specyfikacje testu funkcjonalnego, aponadto przystosowano go do uzycia modulu ninitest

--- functional_tests.py
+++ functional_tests.py
@@ -2,8 +2,27 @@
 # Functional tests
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-binary = FirefoxBinary('/opt/firefox/firefox/firefox')
-browser = webdriver.Firefox(firefox_binary=binary)
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorsTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.binary = FirefoxBinary('/opt/firefox/firefox/firefox')
+		self.browser = webdriver.Firefox(firefox_binary=self.binary)
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_cat_start_a_list_and_retrive_it_later(self):
+		self.browser.get('http://localhost:8000')
+		
+		# Test uruchomionego default django
+		#assert 'Django' in browser.title
+
+		# Tytul strony
+		self.assertIn('Listy', self.browser.title)
+		self.fail('Zakonczenie testu!')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
