b354a472fdef54372475e6b474ab6679916d964c
K.Bakalarski <devops5000@gmail.com>
2019-05-18 22:42:06 +0200

    Pierwsze uruchomienie plikow w repozytorium test funkcjonalny i podstawowa konfiguracja Django

---
+++ functional_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Functional tests
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+binary = FirefoxBinary('/opt/firefox/firefox/firefox')
+browser = webdriver.Firefox(firefox_binary=binary)
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
